86e86938d6e70327eb25058b8c3333672ad7172c
kuruckina <katerinakurochkina7@gmail.com>
2022-06-14 01:27:33 +0300

    #3

--- Assets/script/Quiz.cs
+++ Assets/script/Quiz.cs
@@ -2,9 +2,9 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
 
 public class Quiz : MonoBehaviour
 {
@@ -12,37 +12,74 @@ public class Quiz : MonoBehaviour
     public TextMeshProUGUI QuestionLabel;
     public TextMeshProUGUI[] AnswersLabel;
     public Button[] AnswerButton;
+
+    public Button RestartButton;
+
     private List<object> _qList;
+    private QuestionList _currentQuestion;
     private int _randQuestion;
+    private int _trueUserAnswers;
+    private int _falseUserAnswers;
 
     private void Start()
     {
         _qList = new List<object>(Questions);
         QuestionGenerate();
-        for (int i = 0; i < AnswerButton.Length; i++)
-        {
-            AnswerButton[i].onClick.AddListener(AnswersButtonsMethod);
-        }
+
+        AnswerButton[0].onClick.AddListener(() => AnswersButtonsMethod(0));
+        AnswerButton[1].onClick.AddListener(() => AnswersButtonsMethod(1));
+        AnswerButton[2].onClick.AddListener(() => AnswersButtonsMethod(2));
+        RestartButton.onClick.AddListener(RestartGameScene);
     }
 
     private void QuestionGenerate()
     {
-        _randQuestion = Random.Range(0, _qList.Count);
-        QuestionList currentQuestion = _qList[_randQuestion] as QuestionList;
-        QuestionLabel.text = currentQuestion.Question;
-        for (int i = 0; i < currentQuestion.Answers.Length; i++)
+        if (_qList.Count > 0)
         {
-            AnswersLabel[i].text = currentQuestion.Answers[i];
+            _randQuestion = Random.Range(0, _qList.Count);
+            _currentQuestion = _qList[_randQuestion] as QuestionList;
+            QuestionLabel.text = _currentQuestion.Question;
+            List<string> answers = new List<string>(_currentQuestion.Answers);
+            for (int i = 0; i < _currentQuestion.Answers.Length; i++)
+            {
+                int rand = Random.Range(0, answers.Count);
+                AnswersLabel[i].text = answers[rand];
+                answers.RemoveAt(rand);
+            }
+        }
+        else
+        {
+            RestartButton.gameObject.SetActive(true);
+            QuestionLabel.text = $"Верных ответов: {_trueUserAnswers}\n\n Неверных ответов: {_falseUserAnswers}";
+            for (int i = 0; i < AnswerButton.Length; i++)
+            {
+                AnswerButton[i].gameObject.SetActive(false);
+            }
         }
     }
 
-    private void AnswersButtonsMethod()
+    private void AnswersButtonsMethod(int index)
     {
+        if (AnswersLabel[index].text.ToString() == _currentQuestion.Answers[0])
+        {
+            Debug.Log("Правильный ответ");
+            _trueUserAnswers++;
+        }
+        else
+        {
+            /*AnswerButton[index].GetComponent<Graphic>().color= Color.red;*/
+            Debug.Log("Неправильный ответ");
+            _falseUserAnswers++;
+        }
+
         _qList.RemoveAt(_randQuestion);
         QuestionGenerate();
     }
 
-    //QuestionLabel.text = Questions[Random.Range(0, Questions.Length)].Question;
+    private void RestartGameScene()
+    {
+        SceneManager.LoadScene("Scenes/quiz");
+    }
 }
 
 [System.Serializable]
